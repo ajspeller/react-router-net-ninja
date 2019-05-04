850f1eb34db239dee722764fd0ebf109fce17d54
AJ Speller <ajspeller@gmail.com>
2019-05-03 20:21:12 -0400

    Add redux, reducers and actions

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,8 +1,34 @@
 const initState = {
-  posts: []
+  posts: [
+    {
+      id: '1',
+      title: 'Todd MacFarlane Spider-man',
+      body:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, laboriosam!'
+    },
+    {
+      id: '2',
+      title: 'Avengers End Game',
+      body:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, laboriosam!'
+    },
+    {
+      id: '3',
+      title: 'Avengers Infinity War',
+      body:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, laboriosam!'
+    }
+  ]
 };
 
 const rootReducer = (state = initState, action) => {
+  if (action.type === 'DELETE_POST') {
+    const newPosts = state.posts.filter((p) => p.id !== action.id);
+    return {
+      ...state,
+      posts: newPosts
+    };
+  }
   return state;
 };
 
